73facd617567568843287e4a2f722c50e798b85f
StanDimitroff <standimitroff@gmail.com>
2022-05-03 15:22:04 +0300

    update Utils

--- Sources/Alerts/Utils.swift
+++ Sources/Alerts/Utils.swift
@@ -3,6 +3,6 @@ import UIKit
 class Utils {
 
     static var deviceIsIPAD: Bool {
-        return UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.pad
+      return UIDevice().userInterfaceIdiom == UIUserInterfaceIdiom.pad
     }
 }
